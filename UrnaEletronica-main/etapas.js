cc383848959ec93a2e49777d930e36c22e2626ad
Guilherme-Duartee <115901534+Guilherme-Duartee@users.noreply.github.com>
2023-01-25 18:18:47 -0300

    Add files via upload

---
+++ UrnaEletronica-main/etapas.js
@@ -0,0 +1,62 @@
+let etapas = [
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '13',
+                nome: 'LULA',
+                partido: 'PT',
+                vice: 'Graldo Alckmin',
+                fotos: [
+                    {url: 'lula.jpg', legenda: 'PRESIDENTE'},
+                    {url: 'geraldo.webp', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '22',
+                nome: 'Bolsonaro',
+                partido: 'PL',
+                vice: 'Braga Neto',
+                fotos: [
+                    {url: 'bolso.jpg', legenda: 'PRESIDENTE'},
+                    {url: 'braga.jpeg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            
+        ]
+    },
+
+    {
+        titulo: 'GOVERNADOR',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '25',
+                nome: 'Eduardo Paes',
+                partido: 'PSD',
+                vice: 'Nilton Caldeira',
+                fotos: [
+                    {url: 'paes.png', legenda: 'Prefeito'},
+                    {url: 'nilton.webp', legenda: 'Vice-Prefeito', small: true}
+
+                ]
+            },
+            {
+                numero: '35',
+                nome: 'Wilson Witzel',
+                partido: 'PMB',
+                vice: 'Cláudio Casto',
+                fotos: [
+                    {url: 'wilson.png', legenda: 'Prefeito'},
+                    {url: 'claudio.webp', legenda: 'Vice-Prefeito', small: true}
+
+                ]
+            },
+            
+        ]
+    }
+    
+
+
+]
